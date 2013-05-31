378a23da0f098e1e1f7405d312b0700ba4116212
Parikshit Agarwal <parikshit.ag@hotmail.com>
2013-06-01 00:08:30 +0530

    Prepend gpl licence to h and cpp files

--- src/messagelog.h
+++ src/messagelog.h
@@ -1,3 +1,27 @@
+/****************************************************************************
+**
+** This file is part of Wavr IM Application.
+** 
+** Copyright (c) 2013 Parikshit Agarwal.
+** 
+** Contact:  parikshit.ag@gmail.com
+** 
+** Wavr is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** Wavr is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Wavr.  If not, see <http://www.gnu.org/licenses/>.
+**
+****************************************************************************/
+
+
 #ifndef MESSAGELOG_H
 #define MESSAGELOG_H
 
